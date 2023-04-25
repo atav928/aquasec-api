1e151acc24ed2205425bb726c6fe1cfe9cd50b9d
atav928 <31765990+atav928@users.noreply.github.com>
2023-04-25 14:06:41 -0400

    added some orchestration adn adjustments to get and put and updated rrequirements

--- aquasec/put.py
+++ aquasec/put.py
@@ -2,6 +2,7 @@
 
 from dataclasses import asdict
 from aquasec import config, logger
+from aquasec.statics import DATA_TYPES
 from aquasec.requestapi import aqua_workload_request
 from aquasec.structures import FirewallPolicy
 from aquasec.utilities import UrlUtils
@@ -12,7 +13,24 @@ if not config.SET_LOG:
     aquasec_logger.disabled = True
 
 
-class Post:
+class Put:
     _parent_class = None
     method: str = "PUT"
-    data_types = 
+    data_types: dict = DATA_TYPES
+
+    def workload_protection(self, url_path: str, data_type: str, data: str, **kwargs) -> dict:
+        """Updates an existing object
+
+        Args:
+            url_path (str): _description_
+            data_type (str): _description_
+            data (str): _description_
+            api_version (str): defaults config version otherwise can be adjusted
+
+        Returns:
+            dict: _description_
+        """
+        put_data = asdict(self.data_types[data_type](**data))
+        aquasec_logger.debug("put_data=%s", put_data)
+        api_version = kwargs.pop('api_version',
+                                 self._parent_class.workload_auth.api_version)  # type: ignore
--- aquasec/statics.py
+++ aquasec/statics.py
@@ -3,7 +3,7 @@
 from aquasec.structures import FirewallPolicy
 
 
-BENCH_REPORTS = ['cis', 'kube_bench', 'linux', 'openshift', 'disa_stig', 'all', 'full']
+BENCH_REPORTS = ['cis', 'kube_bench', 'linux', 'openshift', 'disa_stig', 'all']
 DATA_TYPES = {
     "firewall_policy": FirewallPolicy,
 }
