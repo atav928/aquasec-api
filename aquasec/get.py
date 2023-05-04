a8843c7454f865161ff000f9f6be536e35a215fc
atav928 <31765990+atav928@users.noreply.github.com>
2023-05-04 13:39:08 -0400

    Cleanup requests (#27)
    * updatee the config file and remove error in v0.0.3
    
    * added updates to bech report

--- aquasec/get.py
+++ aquasec/get.py
@@ -3,11 +3,11 @@
 
 from aquasec import config, logger
 
-from aquasec.auth import (refresh_workload_token, WorkloadAuth)
 from aquasec.statics import BENCH_REPORTS
-from aquasec.requestapi import (aqua_cloudsploit_request, aqua_workload_request, retrieve_full_list)
+from aquasec.requestapi import (
+    aqua_cloudsploit_request, aqua_workload_request, retrieve_full_list)
 from aquasec.utilities import (reformat_exception, UrlUtils)
-from aquasec.exceptions import (AquaSecWrongParam, AquaSecAPIError)
+from aquasec.exceptions import (AquaSecWrongParam)
 
 logger.addLogger(__name__)
 aquasec_logger = logger.getLogger(__name__)
@@ -34,12 +34,14 @@ class Get:
         Returns:
             dict: JSON results
         """
-        api_version: str = kwargs.pop("api_verison", self._parent_class.api_version)  # type: ignore
+        api_version: str = kwargs.pop(
+            "api_verison", self._parent_class.api_version)  # type: ignore
         url = self._parent_class.cloudsploit_url.format(  # type:ignore
             api_version,
             url_path)
         aquasec_logger.info("Created URL=%s", url)
-        response = aqua_cloudsploit_request(url=url, method=self.method, **kwargs)
+        response = aqua_cloudsploit_request(
+            url=url, method=self.method, **kwargs)
         aquasec_logger.debug("Response=%s", response)
         return response
 
@@ -95,12 +97,15 @@ class Get:
         """
         report_type = report_type.lower()
         if report_type not in BENCH_REPORTS:
-            aquasec_logger.error("AquaSecWrongParam: Invalid report_type value %s", report_type)
-            raise AquaSecWrongParam(f"Invalid report_type value: {report_type=}")
+            aquasec_logger.error(
+                "AquaSecWrongParam: Invalid report_type value %s", report_type)
+            raise AquaSecWrongParam(
+                f"Invalid report_type value: {report_type=}")
         cluster_name: str = kwargs.pop('cluster_name', "")
         hosts: list = self._get_hosts_id_list(cluster_name=cluster_name)
         # TODO: Paralize
-        bench_report: dict = self._get_bench_report(host_id_list=hosts, report_type=report_type)
+        bench_report: dict = self._get_bench_report(
+            host_id_list=hosts, report_type=report_type)
         return bench_report
 
     def _get_hosts_id_list(self, cluster_name: str) -> list:
@@ -123,7 +128,8 @@ class Get:
             error = reformat_exception(err)
             aquasec_logger.error("AquaSecAPIError: %s", error)
         if cluster_name:
-            aquasec_logger.info("Searching for hosts that belong to cluster_name=%s", cluster_name)
+            aquasec_logger.info(
+                "Searching for hosts that belong to cluster_name=%s", cluster_name)
         for _ in all_hosts:
             if cluster_name and _['cluster_name'] == cluster_name:
                 host_id_list.append(_['id'])
@@ -144,17 +150,20 @@ class Get:
         bench_report: dict = {}
         for _ in host_id_list:
             bench_report[_] = self.workload_protection(
-                url_path=config.WORKLOAD_URL_PATHS['host_bench_report']['path'].format(_),
+                url_path=config.WORKLOAD_URL_PATHS['host_bench_report']['path'].format(
+                    _),
                 api_version=config.WORKLOAD_URL_PATHS['host_bench_report']['version'])
-        if report_type in ['all', 'full']:
+        if report_type.lower() in ['all', 'full']:
             aquasec_logger.info("Retrieved full bench report")
             return bench_report
         # Gets specific report
         specific_bench_report: dict = {identifier: {report_type: {}}
                                        for identifier in bench_report}
         for _ in list(bench_report):
-            specific_bench_report[_][f"{report_type}"] = bench_report[_].get(report_type, {})
-        aquasec_logger.info("Retrieved %s_report for specified hosts", report_type)
+            specific_bench_report[_][f"{report_type}"] = bench_report[_].get(
+                report_type, {})
+        aquasec_logger.info(
+            "Retrieved %s_report for specified hosts", report_type)
         return specific_bench_report
 
     def _endpoint(self, url_path: str, **kwargs) -> str:
@@ -168,5 +177,6 @@ class Get:
         """
         api_version: str = kwargs.pop(
             'api_version', self._parent_class.api_version)  # type: ignore # type :ignore
-        url = self._parent_class.cloudsploit_url.format(api_version, url_path)  # type: ignore
+        url = self._parent_class.cloudsploit_url.format(
+            api_version, url_path)  # type: ignore
         return url
